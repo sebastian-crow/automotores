b8154d82e5d588b32f10e60c8328f50a951ab498
sebastian-crow <sebastianberiio@gmail.com>
2023-02-13 18:00:32 -0500

    Register working

--- Login/Register/main.js
+++ Login/Register/main.js
@@ -12,30 +12,51 @@ createApp({
         phoneNumber: "",
       },
       users: [],
+      message: "",
     };
   },
   methods: {
     register() {
-      const user = this.users.find(
-        (user) =>
-          user.rol === this.input.rol && user.document == this.input.document
-      );
       if (
-        this.input.rol === user.rol &&
-        this.input.document === user.document
+        this.input.rol !== "" &&
+        this.input.document !== "" &&
+        this.input.name !== "" &&
+        this.input.lastname !== "" &&
+        this.input.address !== "" &&
+        this.input.phoneNumber !== ""
       ) {
-        alert("El usuario ya se encuentra registrado");
+        const newUser = {
+          rol: this.input.rol,
+          document: this.input.document,
+          name: this.input.name,
+          lastname: this.input.lastname,
+          address: this.input.address,
+          phoneNumber: this.input.phoneNumber,
+        };
+
+        if (this.users?.length > 0) {
+          const user = this.users.find(
+            (user) =>
+              user.document === this.input.document &&
+              user.rol === this.input.rol
+          );
+          if (user) {
+            this.message = "El usuario ya se encuentra registrado";
+            return;
+          }
+
+          localStorage.setItem(
+            "users",
+            JSON.stringify([...this.users, newUser])
+          );
+          alert("Usuario nuevo agregado con exito");
+          return;
+        }
+        localStorage.setItem("users", JSON.stringify([newUser]));
+        alert("Array vacio con un usuario");
+      } else {
+        this.message = "Debes llenar todos los campos";
       }
-      const newUser = {
-        rol: this.input.rol,
-        document: CryptoJS.SHA512(this.input.document),
-        name: this.input.name,
-        lastname: this.input.lastname,
-        address: this.input.address,
-        phoneNumber: this.input.phoneNumber,
-      };
-      const users = [...this.users, newUser];
-      localStorage.setItem("users", JSON.stringify(users));
     },
   },
   mounted() {
